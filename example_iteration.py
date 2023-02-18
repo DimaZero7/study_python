e30004c68868bb427cb3f62ae1b6d6954e0135f6
Dima Zero <dmitry.goptarev@gmail.com>
2023-02-18 15:49:09 +0300

    Добавлен пример работы генираторов

--- example_iteration.py
+++ example_iteration.py
@@ -22,3 +22,25 @@ for item in obj_custom_list:
     print(item)
 for item in obj_custom_list:
     print(item)
+
+
+class CustomRange:
+    def __init__(self, start: int, end: int):
+        self.start = start
+        self.end = end
+
+    def __iter__(self):
+        """Must initialize and return an iterated object"""
+        self.item = self.start
+        return self
+
+    def __next__(self):
+        """Instructions describing the procedure for issuing elements"""
+        if self.item > self.end:
+            raise StopIteration()
+        return_item = self.item
+        self.item += 1
+        return return_item
+
+
+print([x for x in CustomRange(1, 5)])
